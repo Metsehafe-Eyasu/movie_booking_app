eaf19d423274c0934e395443c1843377652060c6
Metsehafe Tesfu <metsehafe.eyasu@aastustudent.edu.et>
2024-02-12 07:38:15 +0300

    fix: home page UI update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,12 +13,14 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Movies'),
       ),
-      body: ListView.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return MovieListItem(movie: movie);
-        },
+      body: GridView.count(
+        crossAxisCount: 2, // Two movies per row
+        childAspectRatio: 0.6, // Adjust aspect ratio of each card
+        mainAxisSpacing: 10.0, // Add spacing between cards
+        crossAxisSpacing: 10.0,
+        children: movies
+            .map((movie) => MovieListItem(movie: movie))
+            .toList(), // Create a list of MovieListItem widgets
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => Navigator.pushNamed(
@@ -37,17 +39,48 @@ class MovieListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(movie.title),
-      subtitle: Text(movie.genre),
-      leading: Image.asset(movie.posterImage),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => MovieDetailsPage(movie: movie)),
-        );
-      },
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MovieDetailsPage(movie: movie),
+                  ),
+                );
+              },
+              child: Image.asset(
+                movie.posterImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  movie.genre,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
